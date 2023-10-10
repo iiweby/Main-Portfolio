b4fa8e4a3ae1b7048574f455dd4620326777e817
iiweby <danielparedesmilian@gmail.com>
2023-10-09 19:14:58 -0500

    message

--- js/email.js
+++ js/email.js
@@ -1,12 +1,21 @@
+
 window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
+    const contactForm = document.getElementById('contact-form');
+    const successMessage = document.getElementById('success-message');
+
+    contactForm.addEventListener('submit', function(event) {
         event.preventDefault();
         emailjs.sendForm('service_2vbhiyf', 'template_zfe20mj', this)
-            .then(function() {
-                console.log('SUCCESS!');
+            .then(function(response) {
+                console.log('SUCCESS!', response);
+                // Hide the form, but keep the success message visible
+                contactForm.style.display = 'none';
+                // Show the success message
+                successMessage.classList.remove('hidden');
             }, function(error) {
                 console.log('FAILED...', error);
             });
     });
 }
 
+//'service_2vbhiyf', 'template_zfe20mj'
