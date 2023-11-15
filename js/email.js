da408f964c5e8ad4560e9fb9cf78d5c9e9e71924
iiweby <danielparedesmilian@gmail.com>
2023-11-15 13:52:23 -0600

    Added close button

--- js/email.js
+++ js/email.js
@@ -1,21 +1,22 @@
+window.onload = function () {
+  const contactForm = document.getElementById("contact-form");
+  const successMessage = document.getElementById("success-message");
 
-window.onload = function() {
-    const contactForm = document.getElementById('contact-form');
-    const successMessage = document.getElementById('success-message');
-
-    contactForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        emailjs.sendForm('service_2vbhiyf', 'template_zfe20mj', this)
-            .then(function(response) {
-                console.log('SUCCESS!', response);
-                // Hide the form, but keep the success message visible
-                contactForm.style.display = 'none';
-                // Show the success message
-                successMessage.classList.remove('hidden');
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-    });
-}
+  contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    emailjs.sendForm("service_2vbhiyf", "template_zfe20mj", this).then(
+      function (response) {
+        console.log("SUCCESS!", response);
+        // Hide the form, but keep the success message visible
+        contactForm.style.display = "none";
+        // Show the success message
+        successMessage.classList.remove("hidden");
+      },
+      function (error) {
+        console.log("FAILED...", error);
+      }
+    );
+  });
+};
 
 //'service_2vbhiyf', 'template_zfe20mj'
--- js/script.js
+++ js/script.js
@@ -1,74 +1,80 @@
-const btn = document.getElementById('menu-btn');
-  const nav = document.getElementById('menu');
+const btn = document.getElementById("menu-btn");
+const nav = document.getElementById("menu");
+// const popupSubmission = document.getElementsById("success-message");
 
-  function toggleMenu() {
-    btn.classList.toggle('open');
-    nav.classList.toggle('hidden');
-    nav.classList.toggle('flex');
-  }
+const toggleMenu = () => {
+  btn.classList.toggle("open");
+  nav.classList.toggle("hidden");
+  nav.classList.toggle("flex");
+};
 
-  btn.addEventListener('click', () => {
-    toggleMenu();
-  }); 
+btn.addEventListener("click", () => {
+  toggleMenu();
+});
 
-  // Add event listeners to each link inside the mobile menu to close the menu
-  const mobileMenuLinks = document.querySelectorAll('#menu a');
-  mobileMenuLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      if (nav.classList.contains('flex')) {
-        toggleMenu();
-      }
-    });
-  });
-
-  // Add event listener to the document body to close the menu when clicking outside
-  document.body.addEventListener('click', (event) => {
-    if (!nav.contains(event.target) && event.target !== btn && nav.classList.contains('flex')) {
+// Add event listeners to each link inside the mobile menu to close the menu
+const mobileMenuLinks = document.querySelectorAll("#menu a");
+mobileMenuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (nav.classList.contains("flex")) {
       toggleMenu();
     }
   });
-
-  // Contact Me Pop Up 
-
-  document.addEventListener("DOMContentLoaded", function() {
-    const openModalButton = document.getElementById("openModal");
-    const closeModalButton = document.getElementById("closeModal");
-    const modal = document.getElementById("myModal");
-    const contactMeAnchor = document.getElementById("contactMe"); 
-    const heroContactAnchor = document.getElementById("heroContact"); // New line
-
-    openModalButton.addEventListener("click", function() {
-        modal.classList.remove("hidden");
-    });
-
-    closeModalButton.addEventListener("click", function() {
-        modal.classList.add("hidden");
-    });
-    heroContactAnchor.addEventListener("click", function(event) {
-      event.preventDefault(); // Prevent default anchor behavior
-      modal.classList.remove("hidden");
-  });
-
-    contactMeAnchor.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevent default anchor behavior
-        modal.classList.remove("hidden");
-    });
-
-    // Close the modal if the user clicks outside of it
-    window.addEventListener("click", function(event) {
-        if (event.target === modal) {
-            modal.classList.add("hidden");
-        }
-    });
 });
 
-  window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 0) { 
-      navbar.classList.remove('bg-transparent');
-      navbar.classList.add('bg-blue-500'); 
-    } else {
-      navbar.classList.add('bg-transparent');
-      navbar.classList.remove('bg-blue-500');
+// Add event listener to the document body to close the menu when clicking outside
+document.body.addEventListener("click", (event) => {
+  if (
+    !nav.contains(event.target) &&
+    event.target !== btn &&
+    nav.classList.contains("flex")
+  ) {
+    toggleMenu();
+  }
+});
+
+// Contact Me Pop Up
+
+document.addEventListener("DOMContentLoaded", function () {
+  const openModalButton = document.getElementById("openModal");
+  const closeModalButton = document.getElementById("closeModal");
+  const modal = document.getElementById("myModal");
+  const contactMeAnchor = document.getElementById("contactMe");
+  const heroContactAnchor = document.getElementById("heroContact");
+
+  openModalButton.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+  });
+
+  closeModalButton.addEventListener("click", function () {
+    modal.classList.add("hidden");
+  });
+
+  heroContactAnchor.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent default anchor behavior
+    modal.classList.remove("hidden");
+  });
+
+  contactMeAnchor.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent default anchor behavior
+    modal.classList.remove("hidden");
+  });
+
+  // Close the modal if the user clicks outside of it
+  window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      modal.classList.add("hidden");
     }
   });
+});
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 0) {
+    navbar.classList.remove("bg-transparent");
+    navbar.classList.add("bg-blue-500");
+  } else {
+    navbar.classList.add("bg-transparent");
+    navbar.classList.remove("bg-blue-500");
+  }
+});
